01dd6886d8e1011092a1a21f42c4bd788220ed6d
Will Frankland <50025807+will-frankland@users.noreply.github.com>
2023-02-10 13:39:32 -0500

    add searchText useState and code to render results - tested using useState initial state

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -4,6 +4,7 @@ import { Loader, Card, FormField } from '../components';
 const Home = () => {
   const [loading, setLoading] = useState(false);
   const [allPosts, setAllPosts] = useState(null);
+  const [searchText, setSearchText] = useState('');
 
   return (
     <section className='max-w-7xl mx-auto'>
@@ -21,6 +22,11 @@ const Home = () => {
           </div>
         ) : (
           <>
+            {searchText && (
+              <h2 className='font-medium text-[#666e75] text-xl mb-3'>
+                Showing results for <span className='text-[#222328]'>{searchText}</span>
+              </h2>
+            )}
           </>
         )}
       </div>
