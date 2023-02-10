4143cb628079523dbbfcfafac257b858ee2171e9
Will Frankland <50025807+will-frankland@users.noreply.github.com>
2023-02-10 14:11:36 -0500

    create handleChange function

--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -17,6 +17,10 @@ const CreatePost = () => {
 
   };
 
+  const handleChange = (e) => {
+
+  };
+
   return (
     <section className="max-w-7xl mx-auto">
       <div>
@@ -27,7 +31,16 @@ const CreatePost = () => {
         </p>
       </div>
       <form className="mt-16 max-w-3xl" onSubmit={handleSubmit}>
-
+        <div className="flex flex-col gap-5">
+          <FormField
+            LabelName="Your name"
+            type='text'
+            name='name'
+            placeholder='John Doe'
+            value={form.name}
+            handleChange={handleChange}
+          />
+        </div>
       </form>
     </section>
   );
