4a363c3eb87d5dc9e8d2d1f8dcaec267176d2741
Will Frankland <50025807+will-frankland@users.noreply.github.com>
2023-02-10 14:37:14 -0500

    add label and button to FormField

--- client/src/components/FormField.jsx
+++ client/src/components/FormField.jsx
@@ -1,13 +1,36 @@
-import React from 'react';
+import React from "react";
 
-const FormField = ({ LabelName, type, name, placeholder, value, handleChange, isSupriseMe, handleSurpriseMe }) => {
+const FormField = ({
+  LabelName,
+  type,
+  name,
+  placeholder,
+  value,
+  handleChange,
+  isSupriseMe,
+  handleSurpriseMe,
+}) => {
   return (
     <div>
-      <div className='flex items-center gap-2 mb-2'>
-
+      <div className="flex items-center gap-2 mb-2">
+        <label
+          htmlFor={name}
+          className="block text-sm font-medium text-gray-900"
+        >
+          {LabelName}
+        </label>
+        {isSupriseMe && (
+          <button
+            type="button"
+            onClick={handleSurpriseMe}
+            className="font-semibold text-xs bg-[#ECECF1] py-1 px-2 rounded-[5px] text-black"
+          >
+            Suprise Me
+          </button>
+        )}
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default FormField
+export default FormField;
