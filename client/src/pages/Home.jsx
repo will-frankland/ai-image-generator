b1fd7dce10dbb3142cff8e469944cfc6d46b1b04
Will Frankland <50025807+will-frankland@users.noreply.github.com>
2023-02-09 22:26:56 -0500

    add two useStates to home.jsx - loading and posts

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -2,6 +2,9 @@ import React, { useState, useEffect } from 'react';
 import { Loader, Card, FormField } from '../components';
 
 const Home = () => {
+  const [loading, setLoading] = useState(false);
+  const [allPosts, setAllPosts] = useState(null);
+
   return (
     <div>
       Home
