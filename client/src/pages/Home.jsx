4ba77805a0f5fffeb2fd21ebfd630acfe33764e1
Will Frankland <50025807+will-frankland@users.noreply.github.com>
2023-02-10 13:48:05 -0500

    add condtional operator to Render Cards components in render section

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -45,7 +45,19 @@ const Home = () => {
                 <span className="text-[#222328]">{searchText}</span>
               </h2>
             )}
-            <div className="grid lg:grid-cols-4 sm:grid-cols-3 xs:grid-cols-2 grid-cols-1 gap-3"></div>
+            <div className="grid lg:grid-cols-4 sm:grid-cols-3 xs:grid-cols-2 grid-cols-1 gap-3">
+              {searchText ? (
+                <RenderCards 
+                  data='searchedResults'
+                  title='No search results found'
+                  />
+              ) : (
+                <RenderCards 
+                  data="allPosts"
+                  title='No posts found'
+                />
+              )}
+            </div>
           </>
         )}
       </div>
