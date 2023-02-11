3cd791376e37db9f42f157e98109d512d51cfd7f
Will Frankland <50025807+will-frankland@users.noreply.github.com>
2023-02-11 15:31:50 -0500

    setup mongoDB and add URL to .env file - modified import to connect.js as we are running node

--- server/index.js
+++ server/index.js
@@ -1,18 +1,26 @@
 import express from "express";
-import * as dotenv from 'dotenv';
-import cors from 'cors';
+import * as dotenv from "dotenv";
+import cors from "cors";
+import connectDB from "./mongodb/connect.js";
 
 dotenv.config();
 
 const app = express();
 app.use(cors());
-app.use(express.json({ limit: '50mb' }));
+app.use(express.json({ limit: "50mb" }));
 
-app.get('/', async(req, res) => {
-  res.send('Hello from DALL-E!');
-})
+app.get("/", async (req, res) => {
+  res.send("Hello from DALL-E!");
+});
 
 const startServer = async () => {
-  app.listen(8080, () => console.log('Server has started on http://localhost:8080'))
-}
+  try {
+    connectDB(process.env.MONGODB_URL);
+    app.listen(8080, () =>
+      console.log("Server has started on http://localhost:8080")
+    );
+  } catch (error) {
+    console.log(error);
+  }
+};
 startServer();
