2d94927e326caf20a43306a1b58f5c597a9178c4
Will Frankland <50025807+will-frankland@users.noreply.github.com>
2023-02-11 15:18:15 -0500

    create startServer function

--- server/index.js
+++ server/index.js
@@ -11,3 +11,8 @@ app.use(express.json({ limit: '50mb' }));
 app.get('/', async(req, res) => {
   res.send('Hello from DALL-E!');
 })
+
+const startServer = async () => {
+  app.listen(8080, () => console.log('Server has started on http://localhost:8080'))
+}
+startServer();
