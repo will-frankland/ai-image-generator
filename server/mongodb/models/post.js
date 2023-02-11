63c2d498aa656d9187b18b2a92f535e9d8f070d8
Will Frankland <50025807+will-frankland@users.noreply.github.com>
2023-02-11 15:34:24 -0500

    create new model directory and add post file, setup Post function to use when creating new posts

---
+++ server/mongodb/models/post.js
@@ -0,0 +1,12 @@
+import mongoose, { modelNames } from "mongoose";
+
+const Post = new mongoose.Schema({
+  name: { type: String, required: true},
+  prompt: { type: String, required: true},
+  photo: { type: String, required: true}
+});
+
+const PostSchema = mongoose.model('Post', post);
+
+export default PostSchema
+
